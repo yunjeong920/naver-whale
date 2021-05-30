377e67660bb48b758c6d2af9094ddd887479635b
yunjeong <yunjeong920@naver.com>
2021-05-30 23:50:07 +0900

    서브 페이지 1 완성

--- js/common.js
+++ js/common.js
@@ -17,3 +17,13 @@ $(function(){
 //     $("on").css("opacity","1");
 //   }
 // })
+
+$(function(){
+  $("ul.tabs li").click(function(){
+    var tab_id = $(this).attr("data-tab");
+    $("ul.tabs li").removeClass("current");
+    $(".tab-content").removeClass("current");
+    $(this).addClass("current");
+    $("#"+tab_id).addClass("current");
+  })
+})
